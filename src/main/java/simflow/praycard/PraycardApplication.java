01604899da33ab6c21dde21973fdf663aa079e23
jesusism <thejesusism@gmail.com>
2021-07-02 17:33:15 +0900

    Initial commit

---
+++ src/main/java/simflow/praycard/PraycardApplication.java
@@ -0,0 +1,13 @@
+package simflow.praycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraycardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraycardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/simflow/praycard/PraycardApplicationTests.java
@@ -0,0 +1,13 @@
+package simflow.praycard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraycardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
