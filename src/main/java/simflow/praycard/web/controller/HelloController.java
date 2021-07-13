e6f94fa67881b0ccaee08c5488ce718ada5c5c3b
jesusism <thejesusism@gmail.com>
2021-07-14 00:41:02 +0900

    게시판 crud

--- src/main/java/simflow/praycard/web/controller/HelloController.java
+++ src/main/java/simflow/praycard/web/controller/HelloController.java
@@ -1,4 +1,4 @@
-package simflow.praycard.web;
+package simflow.praycard.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
